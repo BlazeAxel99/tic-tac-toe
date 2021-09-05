f65c85229b418714695c0a740c710b5bfb4d931e
blazeaxel99 <kmoulik99@gmail.com>
2021-09-05 21:54:51 +0530

    winner.js

--- src/components/board.js
+++ src/components/board.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Square from './square';
 
-const board = () => {
+const board = ({ board, handleSquareClick }) => {
+  const renderSquare = position => {
+    return (
+      <Square
+        value={board[position]}
+        onClick={() => {
+          handleSquareClick(position);
+        }}
+      />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={0} />
-        <Square value={1} />
-        <Square value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
 
       <div className="board-row">
-        <Square value={3} />
-        <Square value={4} />
-        <Square value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={6} />
-        <Square value={5} />
-        <Square value={4} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
